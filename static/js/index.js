4072ca0105463e918a28bdc7826f54c7ef4e44e7
rafael <rafael@eyequant.com>
2014-09-21 20:44:00 +0200

    Added smooth scrolling.
    - Also some minor formatting changes.

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 var jumboHeight = $('.jumbotron').outerHeight();
-console.log(jumboHeight)
+
 function parallax(){
     var scrolled = $(window).scrollTop();
     $('.bg').css('height', (jumboHeight-scrolled) + 'px');
@@ -7,4 +7,31 @@ function parallax(){
 
 $(window).scroll(function(e){
     parallax();
+});
+
+function smoothScroll(navItem, e){
+   // prevent default anchor click behavior
+     e.preventDefault();
+
+     // animate
+     $('html, body').animate({
+         scrollTop: $(navItem.hash).offset().top
+       }, 500, function(){
+
+         // when done, add hash to url
+         // (default click behaviour)
+         window.location.hash = navItem.hash;
+       });
+}
+
+$(document).ready(function(e) {
+
+  $(".nav li a").on('click', function(e) {
+    smoothScroll(this, e);
+  });
+
+  $(".navbar-brand").on('click', function(e) {
+    smoothScroll(this, e);
+  });
+
 });
